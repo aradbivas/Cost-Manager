670907b91fe3799414d3bd29a342be90fa657c22
Arad Bivas <41302200+aradbivas@users.noreply.github.com>
2022-06-17 14:59:57 +0300

    Update Submit.js

--- front/my-app/src/Submit.js
+++ front/my-app/src/Submit.js
@@ -31,7 +31,7 @@ setValue(prevalue=>({...prevalue,
   
   const handleSubmit=(e)=>{
     e.preventDefault();
-    const res=fetch("http://54.209.76.14:4030/submit",{
+    const res=fetch("http://54.209.76.14:4020/submit",{
       method: 'POST',
       headers:{          
         'Content-Type': 'application/json; charset=utf-8'},
