5cd71c742d3e7c5842ae10dc72b68ad3be3eb609
Arad Bivas <41302200+aradbivas@users.noreply.github.com>
2022-06-22 17:52:22 +0300

    Update server.js

--- CostManager/server.js
+++ CostManager/server.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 const express = require("express");
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const cors = require('cors')
+
 const app = express();
 app.use(express.json());
-const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }));
-const mongoose = require('mongoose');
+app.use(cors())
 app.use(express.json({
     type: ['application/json', 'text/plain']
 }))
-const cors = require('cors')
-
-app.use(cors())
 
 mongoose.connect("mongodb+srv://" + process.env.password +"@cluster0.rfa43.mongodb.net/Costs");
 
@@ -18,13 +18,6 @@ const costsSchema = {
     id:String,
     description:String,
     sum:Number,
-    year:String,
-    month:String
-};
-const reportSchema = {
-    year:String,
-    month:String,
-    cost : []
 };
 const categorySchema = {
     name:String,
@@ -32,12 +25,11 @@ const categorySchema = {
     total: Number
 };
 
-const Cost = mongoose.model("costs", costsSchema);
-const Reports = mongoose.model("reports", reportSchema);
-
-
-const Category = mongoose.model("category", categorySchema);
-
+const reportSchema = {
+    year:String,
+    month:String,
+    category: [categorySchema]
+};
 
 const userSchema = new mongoose.Schema ({
     id:String,
@@ -45,12 +37,15 @@ const userSchema = new mongoose.Schema ({
     last_Name: String,
     BirthDay:Date,
     marital_status:String,
-    category: [categorySchema],
-    report : [reportSchema]
+    report: [reportSchema]
 });
 
-const User = new mongoose.model("User", userSchema);
 
+const Cost = mongoose.model("costs", costsSchema);
+const Reports = mongoose.model("reports", reportSchema);
+const Category = mongoose.model("category", categorySchema);
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/report/:year/:month/:userID", function(req,res)
 {
@@ -64,27 +59,27 @@ app.get("/report/:year/:month/:userID", function(req,res)
             if(foundUser != null)
             {
                 const arr = [];
-
                 var totalSum = 0;
+
                 for(let i = 0; i < foundUser.report.length; i++)
                 {
                     if (foundUser.report[i].year == year && foundUser.report[i].month == month) {
-                        for (let j = 0; j < foundUser.report[i].cost.length; j++) {
-                            if (foundUser.report[i].cost[j].year === year && foundUser.report[i].cost[j].month === month) {
-                                arr.push({
-                                    description: foundUser.report[i].cost[j].description,
-                                    sum: foundUser.report[i].cost[j].sum
-                                });
-                                totalSum += foundUser.report[i].cost[j].sum;
 
-                            }
+                        for (let j = 0; j < foundUser.report[i].category.length; j++) {
+                            for (let k = 0; k < foundUser.report[i].category[j].cost.length; k++) {
+
+                                arr.push({
+                                    description: foundUser.report[i].category[j].cost[k].description,
+                                    sum: foundUser.report[i].category[j].cost[k].sum,
+                                    category: foundUser.report[i].category[j].name
+                                });
+                                totalSum += foundUser.report[i].category[j].cost[k].sum;
                         }
+                    }
                     }
 
                 }
-                arr.push({
-                    totalSum:totalSum
-                });
+                arr.push({totalSum:totalSum});
 
                 if(totalSum != 0)
                 {
@@ -93,7 +88,6 @@ app.get("/report/:year/:month/:userID", function(req,res)
                 else
                 {
                     res.status(400).json('no data on year/month')
-
                 }
             }
             else
@@ -108,26 +102,26 @@ app.get("/report/:year/:month/:userID", function(req,res)
 });
 
 app.post("/adduser", function(req, res){
+
     const user = new User({
         id:req.body.id,
         first_Name: req.body.firstName,
         last_Name: req.body.lastName,
         BirthDay: req.body.BirthDay,
         marital_status: req.body.maritalStatus,
-        category: [],
         report : []
 
     });
+
     User.findOne({id:user.id},function (error,foundUser){
+
         if(!error)
         {
             if(foundUser != null)
             {
-
-                res.status(400).json("user not found")///msg here to client
+                res.status(400).json("user not found")
             }
             else{
-
                 user.save();
                 res.status(201).json("created");
 
@@ -147,15 +141,13 @@ app.post("/submit", function(req, res){
     const totalSum = req.body.sum;
     const costCategory = req.body.category;
     const dateObj = new Date();
-    const month = dateObj.getUTCMonth() + 1; //months from 1-12
+    const month = dateObj.getUTCMonth() + 1;
     const year = dateObj.getUTCFullYear();
 
     const newItem = new Cost({
         id:costId,
         description:costDescription,
         sum:totalSum,
-        year:year,
-        month:month
     });
 
     User.findOne({id:newItem.id}, function (error,foundId){
@@ -163,20 +155,25 @@ app.post("/submit", function(req, res){
         {
             if(foundId === null)
             {
-                res.status(400).json("user not found")
+                res.status(400).json("user not found");
             }
-            else{
-                const temp = foundId.category.filter((category) => {return category.name === costCategory;})
-                if(temp.length!=0)
+            else{               
+             
+                const foundReport = foundId.report.filter((report) => {return report.year == year && report.month == month;})
+                if(foundReport.length!=0)
+                {
+                    const foundCategory = foundReport[0].category.filter((category) => {return category.name === costCategory;})
+                    if(foundCategory.length!=0)
                 {
                     let sum = 0;
-                    temp[0].cost.push(newItem);
-                    for(let i = 0; i < temp[0].cost.length; i++)
+                    foundCategory[0].cost.push(newItem);
+                    for(let i = 0; i < foundCategory[0].cost.length; i++)
                     {
-                        sum +=temp[0].cost[i].sum;
+                        sum +=foundCategory[0].cost[i].sum;
                     }
-                    temp[0].total = sum;
+                    foundCategory[0].total = sum;
                 }
+
                 else{
                     const category = new Category({
                             name: costCategory,
@@ -184,24 +181,30 @@ app.post("/submit", function(req, res){
                             total: newItem.sum
                         }
                     )
-                    foundId.category.push(category);
-                }
-                const reportTemp = foundId.report.filter((report) => {return report.year == year && report.month == month;})
-                if(reportTemp.length!=0)
-                {
-                    let sum = 0;
-                    reportTemp[0].cost.push(newItem);
-                    for(let i = 0; i < reportTemp[0].cost.length; i++)
-                    {
-                        sum +=reportTemp[0].cost[i].sum;
+                    foundReport[0].category.push(category);
                     }
-                    reportTemp[0].total = sum;
+                    
+                    let sum = 0;
+                    
+                    for(let i = 0; i < foundReport[0].category[0].cost.length; i++)
+                    {
+                        sum +=foundReport[0].category[0].cost[i].sum;
+                    }
+                    
+                    foundReport[0].total = sum;
                 }
                 else{
+                    const category = new Category({
+                        name: costCategory,
+                        cost: [],
+                        total: newItem.sum
+                    }
+                )
+                category.cost.push(newItem);
                     const Report = new Reports({
                             year: year,
                         month:month,
-                            cost: [newItem],
+                        category: [category],
                         }
                     )
                     foundId.report.push(Report);
@@ -215,6 +218,6 @@ app.post("/submit", function(req, res){
     });
 });
 
-app.listen(4000, function() {
-    console.log("Server started on port 4000");
+app.listen(8000, function() {
+    console.log("Server started on port 8000");
 });
